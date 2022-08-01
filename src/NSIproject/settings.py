13cbd87bc978cf0bd9b2f4a9fffd34826b2bb1be
maniche <48153321+bmaniche@users.noreply.github.com>
2022-08-01 08:30:37 +0200

    Update settings.py

--- src/NSIproject/settings.py
+++ src/NSIproject/settings.py
@@ -75,9 +75,7 @@ WSGI_APPLICATION = 'NSIproject.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-#DATABASES = {
  #   'default': {
    #     'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',
@@ -116,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/4.0/topics/i18n/
+
 
 LANGUAGE_CODE = 'en-us'
 
@@ -128,7 +126,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
 
 STATICFILES_DIRS = [
     BASE_DIR, "static"
@@ -139,7 +137,7 @@ STATIC_URL = 'static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
